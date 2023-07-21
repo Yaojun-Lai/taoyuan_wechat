0c49f5c04754c23da891efba97ce0cd363851ec4
Yaojun-Lai <al122@rice.edu>
2023-07-20 20:23:58 -0500

    prefer data add new character xiaobai; move the mainIndex data as a seperate js file

--- pages/mainIndex/mainIndex.js
+++ pages/mainIndex/mainIndex.js
@@ -1,23 +1,12 @@
+var mainIndexData = require('../../data/mainIndex.js');
 
 Page({
   data: {
     isDropdownOpen: false,
     showModal: false,
     pageIndex: 0,
-    pageArray: ['---- 攻略 ----','时刻对应表','摸鱼版订单成本','生产建筑','倦鸟汀','千灯彩树', '村民喜好', '花谱', '钓鱼', '萝萝心愿建筑','故事材料','食谱'],
-    pageMap: {
-      '时刻对应表':'/pages/time/time',
-      '生产建筑': '/pages/building2/building2',
-      '摸鱼版订单成本':'/pages/indexplain/indexplain',
-      '倦鸟汀':'/pages/bird/bird',
-      '食谱': '/pages/manual/manual',
-      '村民喜好': '/pages/prefer/prefer',
-      '花谱':'/pages/flower/flower',
-      '萝萝心愿建筑':'/pages/building/building', 
-      '钓鱼':'/pages/fish/fish', 
-      '故事材料':'/pages/storyMaterial/storyMaterial',
-      '千灯彩树':''
-    }
+    pageArray: mainIndexData.pageArray,
+    pageMap: mainIndexData.pageMap,
   },
   showModal: function() {
     this.setData({
